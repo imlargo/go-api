da79dd6204a7101856afed85b8c428e667297be8
imlargo <jlargob@unal.edu.co>
2025-08-17 22:54:15 -0500

    _

--- internal/dto/requests/user.go
+++ internal/dto/requests/user.go
@@ -1,9 +1,7 @@
 package requestsdto
 
 type RegisterUserRequest struct {
-	Name         string `json:"name"`
-	Email        string `json:"email"`
-	Password     string `json:"password"`
-	CreatedBy    uint   `json:"created_by"`
-	ReferralCode string `json:"referral_code"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
