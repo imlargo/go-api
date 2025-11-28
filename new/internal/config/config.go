866c1f2751518925fad9ecfe1bdc94ecfb54c239
imlargo <jlargob@unal.edu.co>
2025-11-27 21:29:54 -0500

    Add configuration loading and environment variable definitions

--- new/internal/config/config.go
+++ new/internal/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+import (
+	"time"
+
+	"github.com/imlargo/go-api/pkg/app"
+	"github.com/imlargo/go-api/pkg/env"
+)
+
+type Config struct {
+	app.Config
+}
+
+func LoadConfig() Config {
+	err := env.CheckEnv([]string{
+		API_URL,
+		PORT,
+		DATABASE_URL,
+		JWT_SECRET,
+		JWT_TOKEN_EXPIRATION,
+		JWT_REFRESH_EXPIRATION,
+	})
+
+	if err != nil {
+		panic("Error loading environment variables: " + err.Error())
+	}
+
+	return Config{
+		Config: app.Config{
+			Server: app.ServerConfig{
+				Host: env.GetEnvString(API_URL, "localhost"),
+				Port: env.GetEnvString(PORT, "8000"),
+			},
+			Database: app.DbConfig{
+				URL: env.GetEnvString(DATABASE_URL, ""),
+			},
+			Auth: app.AuthConfig{
+				JwtSecret:         env.GetEnvString(JWT_SECRET, "your-secret-key"),
+				TokenExpiration:   time.Duration(env.GetEnvInt(JWT_TOKEN_EXPIRATION, 15)) * time.Minute,
+				RefreshExpiration: time.Duration(env.GetEnvInt(JWT_REFRESH_EXPIRATION, 10080)) * time.Minute,
+			},
+		},
+	}
+}
---
+++ new/internal/config/env.go
@@ -0,0 +1,12 @@
+package config
+
+// Define enums for environment variable keys
+const (
+	API_URL      = "API_URL"
+	PORT         = "PORT"
+	DATABASE_URL = "DATABASE_URL"
+
+	JWT_SECRET             = "JWT_SECRET"
+	JWT_TOKEN_EXPIRATION   = "JWT_TOKEN_EXPIRATION"
+	JWT_REFRESH_EXPIRATION = "JWT_REFRESH_EXPIRATION"
+)
