69c1ce524485ecc9020e1eb2fbf5c57a5073de25
imlargo <jlargob@unal.edu.co>
2025-12-07 12:27:19 -0500

    Implement main function and logger initialization for the API

--- new/cmd/api/main.go
+++ new/cmd/api/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/imlargo/go-api/pkg/medusa/core/app"
+	"github.com/imlargo/go-api/pkg/medusa/core/logger"
+	"github.com/imlargo/go-api/pkg/medusa/core/server/http"
+)
+
+func main() {
+
+	logger := logger.NewLogger()
+
+	engine := gin.Default()
+	srv := http.NewServer(
+		engine,
+		logger,
+		http.WithServerHost("localhost"),
+		http.WithServerPort(8080),
+	)
+
+	app := app.NewApp(
+		app.WithName("butter"),
+		app.WithServer(srv),
+	)
+
+	app.Run(context.Background())
+}
--- new/pkg/medusa/core/logger/logger.go
+++ new/pkg/medusa/core/logger/logger.go
@@ -5,3 +5,8 @@ import "go.uber.org/zap"
 type Logger struct {
 	*zap.Logger
 }
+
+func NewLogger() *Logger {
+	logger, _ := zap.NewProduction()
+	return &Logger{Logger: logger}
+}
