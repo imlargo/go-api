a022fc16f2b68cbc99e229789fc4fc797763a495
imlargo <jlargob@unal.edu.co>
2025-12-13 11:02:10 -0500

    Refactor ping endpoint to use responses package for consistent success response

--- new/cmd/api/main.go
+++ new/cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/imlargo/go-api/internal/config"
 	"github.com/imlargo/go-api/pkg/medusa/core/app"
 	"github.com/imlargo/go-api/pkg/medusa/core/logger"
+	"github.com/imlargo/go-api/pkg/medusa/core/responses"
 	"github.com/imlargo/go-api/pkg/medusa/core/server/http"
 )
 
@@ -24,17 +25,20 @@ func main() {
 		http.WithServerPort(cfg.Server.Port),
 	)
 
-	// MOUNT APP
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
 	app := app.NewApp(
 		app.WithName("butter"),
 		app.WithServer(srv),
 	)
 
+	Mount(app, cfg, router, logger)
+
 	app.Run(context.Background())
+}
+
+func Mount(app *app.App, cfg config.Config, router *gin.Engine, logger *logger.Logger) {
+
+	// Ping
+	router.GET("/ping", func(c *gin.Context) {
+		responses.SuccessOK(c, "hello")
+	})
 }
