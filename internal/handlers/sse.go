61f6008eca0cb7656ab5c7f3188d292bf11a027f
imlargo <jlargob@unal.edu.co>
2025-12-13 11:28:25 -0500

    Implement SSE handler with Listen and Publish methods, and update main to route SSE endpoints

--- internal/handlers/sse.go
+++ internal/handlers/sse.go
@@ -1,13 +1,22 @@
 package handlers
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/imlargo/go-api/pkg/medusa/core/handler"
 )
 
 type Handler struct {
-	handler.Handler
+	*handler.Handler
 }
 
-func NewHandler(handler handler.Handler) *Handler {
+func NewSSEHandler(handler *handler.Handler) *Handler {
 	return &Handler{Handler: handler}
+}
+
+func (h *Handler) Listen(c *gin.Context) {
+
+}
+
+func (h *Handler) Publish(c *gin.Context) {
+
 }
